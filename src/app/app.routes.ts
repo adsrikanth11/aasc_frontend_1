2be0d35c21e3d0d9dd495c5726b9141e63645f3a
Srikanth <adsrikanth11@gmail.com>
2025-07-18 16:29:15 +0530

    Implement Home, student registration and Page Not Found components with routing; add styles and tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {  
+      path: '', 
+      loadComponent: () => import('./feature/home/home.component').then(m => m.HomeComponent) 
+   },
+   { 
+      path: 'registration', 
+      loadComponent: () => import('./feature/student-registration/student-registration.component').then(m => m.StudentRegistrationComponent) 
+   },
+   { 
+      path: '**', 
+      loadComponent: () => import('./feature/pagenotfound/pagenotfound.component').then(m => m.PagenotfoundComponent) 
+   }
+];
---
+++ src/app/feature/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../core/material/material.module';
+
+@Component({
+  selector: 'app-home',
+  imports: [MaterialModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
